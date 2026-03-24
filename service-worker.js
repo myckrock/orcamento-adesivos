a29b3c75e92153af02296ded90a12740ee988aba
myckrock <99827610+myckrock@users.noreply.github.com>
2026-03-23 22:44:22 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,46 @@
-const CACHE_NAME = "orcamento-v8";
+const CACHE_NAME = "orcamento-app-v2"; // MUDE A VERSÃO SEMPRE
+
 const urlsToCache = [
-  "index.html",
-  "manifest.json"
+  "/",
+  "/index.html",
+  "/manifest.json"
 ];
 
+// INSTALAÇÃO
 self.addEventListener("install", event => {
+  self.skipWaiting(); // força atualizar
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+// ATIVAÇÃO
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+  self.clients.claim();
 });
 
-
+// FETCH (sempre tenta pegar versão nova primeiro)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
